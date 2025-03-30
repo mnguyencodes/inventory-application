94f35bce7062d0ace20e2fda7fbdc98100373a24
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-30 14:33:49 -0600

    Add Footer components
    Added:
    - Footer.tsx
    - Footer.css

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import "./Footer.css"
+
+export default function() {
+    return (
+        <footer>
+            <h2>Inventory Application © {new Date().getFullYear()}</h2>
+        </footer>
+    )
+}
