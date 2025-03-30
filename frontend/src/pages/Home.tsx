602a16ce3d1639c824bee9a233de4037988298d4
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-30 14:49:11 -0600

    Add Homepage

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import "./Home.css"
+
+
+export default function Home() {
+    return (
+        <>
+            <h1 className="title">Welcome to my Inventory Application</h1>
+        </>
+    )
+}
