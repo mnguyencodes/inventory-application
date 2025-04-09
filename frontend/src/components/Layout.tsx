fe3f2fc32af3f18a5372aeee59afaee56714d0dc
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-08 20:21:18 -0600

    Refactor Layout.tsx components
    - Refactored Layout.tsx component.
    - Imported the following components into Layout.tsx:
        - AppShellInit
        - Header
        - Navbar
        - OutletInit
        - Footer
    
    By writing the code like this, we have abstracted each component used into their own separate modules.

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,32 +1,21 @@
-import {Outlet} from "react-router"
+import AppShellInit from "./AppShellInit"
 import Header from "./Header"
+import Navbar from "./Navbar"
+import OutletInit from "./OutletInit"
 import Footer from "./Footer"
-import {AppShell} from "@mantine/core"
-import {useDisclosure} from "@mantine/hooks"
-import styles from "./Layout.module.css"
+
+// import styles from "./_styles/Layout.module.css"
 
 export default function Layout() {
 
-    const [opened, { toggle }] = useDisclosure()
-
     return (
         <>
-            <AppShell
-                header={{ height: 60 }}
-                footer={{ height: 60 }}
-                navbar={{
-                    width: {sm: 200, lg: 250},
-                    breakpoint: 'sm',
-                    collapsed: { mobile: !opened },
-                }}
-                padding={{ base: 10, sm: 15, lg: 'xl' }}
-            >
-                <Header toggle={toggle} opened={opened} />
-                <AppShell.Main className={styles.main}>
-                    <Outlet />
-                </AppShell.Main>
+            <AppShellInit>
+                <Header />
+                <Navbar />
+                <OutletInit />
                 <Footer />
-            </AppShell>
+            </AppShellInit>
         </>  
     )
 }
