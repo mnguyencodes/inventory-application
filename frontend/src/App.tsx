54965554a310ab366aef86b32eb49a0246ed4830
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-10 07:09:15 -0600

    Import _global.css
    - Imported _global.css in the following files:
        - App.tsx
        - main.tsx
        - Home.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import "@mantine/core/styles.css"
 import { MantineProvider } from "@mantine/core"
-import './App.css'
+import './_global.css'
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import {BrowserRouter, Routes, Route} from "react-router"
