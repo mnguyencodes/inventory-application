69c418cd0a70b33b4e3f55c4f99237679555bd65
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-11 16:03:24 -0600

    Add links to Navbar component [WIP]
    - Added links to Navbar component
    - Will hardcode links for now and then later map through the links in NavBar-Data.ts

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,32 @@
 import {AppShell} from "@mantine/core";
 import styles from "./_styles/Navbar.module.css"
+import {NavLink} from "@mantine/core"
+import {useState} from "react"
+import navbarData from "./utils/Navbar-Data"
 
 export default function Navbar() {
+    // const [active, setActive] = useState(0)
+
+    // const navbarEl = navbarData.map((navlink, index)=>{
+    //     return <NavLink 
+    //         href={navlink.href}
+    //         key={navlink.label}
+    //         active={index === active}
+    //         leftSection={<navlink.leftSection size={16} stroke={1.5}/>}
+    //         onClick={()=>setActive(index)}
+    //     />
+    // })
+
     return (
-        <AppShell.Navbar className={styles.nav} p="md">Navbar</AppShell.Navbar>
+        <AppShell.Navbar className={styles.nav} p="md">
+            {/* {navbarEl} */}
+            <NavLink href="/" />
+            <NavLink href="/games" />
+            <NavLink leftSection ></NavLink>
+        </AppShell.Navbar>
     )
 }
+
+{/* <NavLink href="/">Home</NavLink>
+<NavLink href="/games">Games</NavLink>
+<NavLink leftSection ></NavLink> */}
