5b55c37432b517586885d2d47f0b7ed371765d9e
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-11 19:40:18 -0600

    Fix initial load issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      // Solution: https://stackoverflow.com/questions/79194970/tabler-icons-for-react-slowing-down-app-on-initial-load
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
 })
