dd496b93f2642250d7913a40f5decd145ccfd53c
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-14 06:42:07 -0600

    Add types to Game component

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,5 +1,20 @@
 import {useFetch} from "@mantine/hooks"
 
+interface Game {
+    title: string
+    year: number
+    genre: Genre[]
+    developer: Developer[]
+}
+
+type Genre = {
+    name: string
+}
+
+type Developer = {
+    name: string
+}
+
 export default function Game() {
     return (
         <h1>Game component goes here!</h1>
