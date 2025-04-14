b102b49f2dd44bd64623fa4215ea321664f44a48
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-14 13:31:16 -0600

    Fix CORS allowed list
    - Fixed CORS allowed list at http://localhost:5173.

--- backend/src/utils/cors.ts
+++ backend/src/utils/cors.ts
@@ -1,7 +1,7 @@
 import {Request} from "express"
 import cors from "cors"
 
-const allowedList: String[] = ["http://localhost:5173/"]
+const allowedList: String[] = ["http://localhost:5173"]
 
 export const corsOptionsDelegate = (req: Request, callback: ((err: Error | null, options?: cors.CorsOptions) => void))=>{
     const corsOptions: cors.CorsOptions = allowedList.indexOf(req.header("Origin")!) !== -1
