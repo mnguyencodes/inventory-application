c8e1bdee2d458ced78cb87e4a9dc98c4c156f2f8
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-22 06:51:29 -0600

    Add comments to implmement usersPost

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,4 +2,13 @@ import queries from '../db/queries'
 
 const usersPost = async (firstName: string, lastName: string, email: string, password: string) => {
   const newUser = await queries.usersPost(firstName, lastName, email, password)
+  // Let's start off by checking if the email exists in the database.
+  // Prisma Docs:
+
+  // By unique identifier
+  // const user = await prisma.user.findUnique({
+  //     where: {
+  //       email: 'elsa@prisma.io',
+  //     },
+  //   })
 }
