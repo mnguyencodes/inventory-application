510ba14d7b6ee08da24a76949cbd66e0c7ebee6e
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-23 06:57:26 -0600

    Add initial logic for duplicate emails
    - Added initial logic for duplicate emails when adding a new user.
    - The database schema will need to be updated.
        - User model needs to have the @unique constraint added to the email field.

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,6 +2,15 @@ import asyncHandler from 'express-async-handler'
 import pool from '../db/pool'
 
 const usersPost = async (firstName: string, lastName: string, email: string, password: string) => {
+  // Need to update database schema to ensure that the @unique constraint is added to email column
+  const duplicateEmail = await pool.user.findUnique({
+    where: {
+      email: email,
+    },
+  })
+  if (duplicateEmail) {
+    // Throw error
+  }
   const newUser = await pool.user.create({
     data: {
       firstName,
