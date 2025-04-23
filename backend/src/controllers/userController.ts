3d8f5d32d6a033113c820d3e53f11c0ac64a81ad
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-23 06:48:47 -0600

    Remove queries dependency from queries.ts

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,9 +1,18 @@
-import queries from '../db/queries'
 import asyncHandler from 'express-async-handler'
 import pool from '../db/pool'
 
 const usersPost = async (firstName: string, lastName: string, email: string, password: string) => {
-  const newUser = await queries.usersPost(firstName, lastName, email, password)
+  const newUser = await pool.user.create({
+    data: {
+      firstName,
+      lastName,
+      email,
+      password,
+    },
+  })
+
+  //   const newUser = await pool.usersPost(firstName, lastName, email, password)
+
   // Let's start off by checking if the email exists in the database.
   // Prisma Docs:
 
