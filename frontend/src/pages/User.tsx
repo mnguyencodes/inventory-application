56f0c5b4b155384ec8a3049dabfcd5f1342a274d
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-27 15:57:39 -0600

    Add User type in User component

--- frontend/src/pages/User.tsx
+++ frontend/src/pages/User.tsx
@@ -1,5 +1,12 @@
 import { useFetch } from '@mantine/hook'
 
+type User = {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+}
+
 export default function User() {
   const getUsers = async () => {}
   return (
