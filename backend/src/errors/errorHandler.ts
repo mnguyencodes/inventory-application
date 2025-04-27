ae9c4f4294f454a7783b9ec12704d31da902fb9d
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-26 18:36:50 -0600

    Add CustomError type

--- backend/src/errors/errorHandler.ts
+++ backend/src/errors/errorHandler.ts
@@ -1,12 +1,11 @@
 import { Request, Response, NextFunction } from 'express'
 
-const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.status(500).send({ errors: err.message })
+interface CustomError extends Error {
+  statusCode: number
+}
+
+const errorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.statusCode || 500).send({ errors: err.message })
 }
 
 export { errorHandler }
