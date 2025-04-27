e1dd3926d1e2655582dbb1ce053ce93965870f30
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-04-27 16:18:37 -0600

    Render User table to DOM

--- frontend/src/pages/User.tsx
+++ frontend/src/pages/User.tsx
@@ -12,9 +12,36 @@ type User = {
 export default function User() {
   const { data, loading, error, refetch, abort } = useFetch<User[]>('http://localhost:3000/users')
 
+  const rowsEl =
+    data &&
+    data.map((user: User) => {
+      return (
+        <Table.Tr key={user.id}>
+          <Table.Td>{user.firstName}</Table.Td>
+          <Table.Td>{user.lastName}</Table.Td>
+          <Table.Td>{user.email}</Table.Td>
+          <Table.Td>{user.password}</Table.Td>
+        </Table.Tr>
+      )
+    })
+
   return (
     <>
       <h1>Users Component</h1>
+
+      {rowsEl && (
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>First Name</Table.Th>
+              <Table.Th>Last Name</Table.Th>
+              <Table.Th>Email</Table.Th>
+              <Table.Th>Password</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rowsEl}</Table.Tbody>
+        </Table>
+      )}
     </>
   )
 }
