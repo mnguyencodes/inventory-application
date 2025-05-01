0c78861404edd5d731d1607e67725d11c7940500
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-05-01 06:35:43 -0600

    Import QueryClient and QueryClientProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css'
-import { MantineProvider } from '@mantine/core'
 import './_global.css'
+import { MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Layout from './components/Layout'
 import Home from './pages/Home'
