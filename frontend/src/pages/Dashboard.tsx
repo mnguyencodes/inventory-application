3220069b4b01ecc4424824a778b2a2b3c9a1f019
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-06-17 07:07:31 -0600

    Refactor Dashboard component
    - Updated `Dashboard.tsx` to conditionally render content based on the user's authentication status.
    - Displays dashboard contents if the user is signed in, otherwise prompts the user to log in.

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,18 @@
 import { useAuth } from '../context/AuthContext'
 
 export default function Dashboard() {
-  return (
-    <div>
-      <h1>Dashboard component here!</h1>
-    </div>
-  )
+  const { isAuthenticated } = useAuth()
+  if (isAuthenticated) {
+    return (
+      <div>
+        <h1>Dashboard component here! You are signed in!</h1>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>Please log in to access the dashboard.</h1>
+      </div>
+    )
+  }
 }
