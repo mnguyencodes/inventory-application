4c043fd570d9cd65711c6310940fe7c3c448532d
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-07-08 07:06:29 -0600

    Add comment explaining need for JWT protection
    Added comments to clarify that Manage Games routes must be protected by JWT authentication, since endpoints are accessible via URL even if the UI link is hidden. This prevents unauthorized access to manage games functionality.

--- backend/src/controllers/manageGamesController.ts
+++ backend/src/controllers/manageGamesController.ts
@@ -1,3 +1,7 @@
+// Note: Even though the Manage Games link is not visible in the UI, it is still accessible via the URL.
+// Thus, we need to ensure that the route is protected by JWT authentication.
+// This will prevent unauthorized access to the manage games functionality.
+
 import { Request, Response } from 'express'
 import asyncHandler from 'express-async-handler'
 import pool from '../db/pool'
