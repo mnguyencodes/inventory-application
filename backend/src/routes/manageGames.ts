cbc8ac9fef4771ed24ebfd4079ef27b589952029
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-07-12 06:43:54 -0600

    Add route setup and export for manageGamesRouter
    Added route definitions for POST and GET requests in `manageGamesRouter`, connecting them to the appropriate controller methods. Exported the router for use in the application.

--- backend/src/routes/manageGames.ts
+++ backend/src/routes/manageGames.ts
@@ -1,2 +1,8 @@
 import { Router } from 'express'
 import manageGamesController from '../controllers/manageGamesController'
+
+const manageGamesRouter = Router()
+manageGamesRouter.post('/', manageGamesController.manageGamesPost)
+manageGamesRouter.get('/', manageGamesController.manageGamesGet)
+
+export default manageGamesRouter
