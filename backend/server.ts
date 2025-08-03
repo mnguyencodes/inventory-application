ffd7a5e8dfa8971dadb50cd0ec4574582ccd5b0f
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-08-03 06:29:20 -0600

    Import manageGamesRouter in server.ts
    Added import for `manageGamesRouter` from `./src/routes/manageGames` to enable routing for manage games functionality in the backend server.

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import { gameRouter } from './src/routes/game'
 import userRouter from './src/routes/user'
 import dashboardRouter from './src/routes/dashboard'
+import manageGamesRouter from './src/routes/manageGames'
 import { corsUtil } from './src/utils/cors'
 import { errorHandler } from './src/errors/errorHandler'
 
